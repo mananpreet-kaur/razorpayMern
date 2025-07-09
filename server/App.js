355eadfdd3590fb87fc07c4495f95f68ca41c55a
mananpreet-kaur <mananpreetkaur2004@gmail.com>
2025-07-09 21:33:08 +0530

    frontend initialization

--- server/App.js
+++ server/App.js
@@ -1,11 +1,11 @@
 
 import express from"express";
-// import { config } from 'dotenv';
+import cors from "cors";
 import paymentRoute from './routes/paymentRoute.js';
-// config({path:"./config/config.env"});
+
 
 export const app= express();
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded request bodies
-
+app.use(cors);
 app.use("/api",paymentRoute);
